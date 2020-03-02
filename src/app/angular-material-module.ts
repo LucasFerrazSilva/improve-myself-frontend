68ee370d3a7d64e5d8e25da5a671497261d7de23
LucasFerrazSilva <lucasferrazsilva@hotmail.com>
2020-03-02 14:50:33 -0300

    Add home component

--- src/app/angular-material-module.ts
+++ src/app/angular-material-module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule
   ]
 })
 export class AngularMaterialModule {}
