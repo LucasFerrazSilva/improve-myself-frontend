1c8af541f6523b8abc6c63cd8c0c37846a261537
LucasFerrazSilva <lucasferrazsilva@hotmail.com>
2020-03-02 11:30:02 -0300

    Add sidenav-container

---
+++ src/app/angular-material-module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatSidenavModule
+  ]
+})
+export class AngularMaterialModule {}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
